42be690aad4680a44153957b51fc55347d91dbb2
Sotatek-DucPham <duc.pham@sotatek.com>
2021-05-23 22:53:54 +0700

    f

--- src/scenarios/deploy_v4.ts
+++ src/scenarios/deploy_v4.ts
@@ -1,12 +1,25 @@
+import { runMigrationsOnceAsync } from '@0x/migrations';
+import { devConstants, web3Factory } from '@0x/dev-utils';
+import { Web3Wrapper } from '@0x/web3-wrapper';
+import { Web3ProviderEngine } from '@0x/subproviders';
 import { providerEngine } from '../provider_engine';
-import { runMigrationsOnceIfRequiredAsync } from '../utils';
 
 /**
  * In this scenario, the maker creates and signs an order for selling ZRX for WETH.
  * The taker takes this order and fills it via the 0x Exchange contract.
  */
 export async function scenarioAsync(): Promise<void> {
-  await runMigrationsOnceIfRequiredAsync();
+  const provider: Web3ProviderEngine = web3Factory.getRpcProvider({
+    shouldUseInProcessGanache: true,
+  });
+  const web3Wrapper = new Web3Wrapper(provider);
+  const txDefaults = {
+    gas: devConstants.GAS_LIMIT,
+    from: devConstants.TESTRPC_FIRST_ADDRESS,
+  };
+
+  const contractAddresses = await runMigrationsOnceAsync(provider, txDefaults);
+  console.log({ contractAddresses });
 }
 
 void (async () => {
